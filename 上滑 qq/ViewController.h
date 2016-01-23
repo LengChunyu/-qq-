5d749414dcee8fc9861459c50aba0d5b13dfe232
lx <lx@lxdeimac.local>
2016-01-23 17:31:05 +0800

    Initial Commit

---
+++ 上滑 qq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  上滑 qq
+//
+//  Created by lx on 16/1/23.
+//  Copyright © 2016年 lx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 上滑 qq/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  上滑 qq
+//
+//  Created by lx on 16/1/23.
+//  Copyright © 2016年 lx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
